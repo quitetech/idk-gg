84d12618b5b6186319b4e02ba69863a5996c1061
quitetech <90640884+quitetech@users.noreply.github.com>
2021-12-03 12:06:52 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import '@expo/browser-polyfill';
+import './three-common';
+
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import Preload from './components/Preload';
+
+/* 
+  Inspired by WebGL tutorial: https://www.airtightinteractive.com/2015/01/building-a-60fps-webgl-game-on-mobile/
+*/
+
+StatusBar.setHidden(true, true);
+
+export default Preload;
